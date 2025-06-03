fe7008dce40d3374be3de570b63cb39aff980e19
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2025-06-03 15:40:00 -0700

    Fix project path

---
+++ HandsFreeNotes/HandsFreeNotes/HandsFreeNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandsFreeNotesApp.swift
+//  HandsFreeNotes
+//
+//  Created by Noah Stull on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HandsFreeNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HandsFreeNotes/HandsFreeNotesTests/HandsFreeNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  HandsFreeNotesTests.swift
+//  HandsFreeNotesTests
+//
+//  Created by Noah Stull on 6/3/25.
+//
+
+import Testing
+@testable import HandsFreeNotes
+
+struct HandsFreeNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
